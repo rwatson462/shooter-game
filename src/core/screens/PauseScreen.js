d57d6a9a9d9c62b3736e8ed3fbd5d2cf251ce8fe
Rob Watson <source.pot.dev@gmail.com>
2026-01-03 20:50:33 +0000

    Remove redundant constructor, stop accidental quitting of game

--- src/core/screens/PauseScreen.js
+++ src/core/screens/PauseScreen.js
@@ -2,22 +2,11 @@ import {Renderer} from "../Renderer.js";
 import {Screen} from "./Screen.js";
 
 export class PauseScreen extends Screen {
-    /**
-     *
-     * @param {Application} application
-     * @param {InputManager} inputManager
-     * @param {number} width
-     * @param {number} height
-     */
-    constructor(application, inputManager, width, height) {
-        super(application, inputManager, width, height)
-    }
-
     update() {
-        if (this.inputManager.isKeyPressed('Space')) {
+        if (this.inputManager.isKeyPressed('Escape')) {
             this.application.setScreen('inGame', false)
         }
-        if (this.inputManager.isKeyPressed('Escape')) {
+        if (this.inputManager.isKeyPressed('KeyQ')) {
             this.application.setScreen('title')
         }
     }
@@ -29,7 +18,7 @@ export class PauseScreen extends Screen {
         renderer.clear('#111')
         renderer.drawText(this.width/2, this.height/2, 'Shooter - paused', '#fff', '48', 'center')
 
-        renderer.drawText(this.width/2, this.height/2 + 30, 'Press [space] to resume', '#99f', '24', 'center')
-        renderer.drawText(this.width/2, this.height/2 + 48, 'Press [esc] to quit', '#f99', '12', 'center')
+        renderer.drawText(this.width/2, this.height/2 + 30, 'Press [esc] to resume', '#99f', '24', 'center')
+        renderer.drawText(this.width/2, this.height/2 + 48, 'Press [q] to quit', '#f99', '12', 'center')
     }
 }
